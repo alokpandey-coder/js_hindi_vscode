bd42a136cbb0380dca12c2ddac16c8af8b7ceb8e
ALOK KUMAR PANDEY <114102706+alokpandey-coder@users.noreply.github.com>
2024-06-11 02:19:44 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version.
+
+//alert("hello") 
+
+//datatypes
+
+/*
+   number  ==> 2 to power 53
+   bigint
+   string ==> ""
+   boolean ==> true/false
+   null ==> standalone value representation of empty value(it can be assigned as null like temp = null)
+   undefined ==> not assigning value
+   symbol ==> unique
+
+   object
+*/
+
+console.log(typeof "ALOK");
+
+console.log(typeof null);
+console.log(typeof undefined);
+
