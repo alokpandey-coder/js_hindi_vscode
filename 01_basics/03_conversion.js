26d495dbc89d7974ade6721acc5f0cfcbdae91e6
ALOK KUMAR PANDEY <114102706+alokpandey-coder@users.noreply.github.com>
2024-06-11 02:41:06 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,20 @@
+let score = "33abcd"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" ==> 33
+// "33abc"  ==> NaN Not A Number
+//  true ==> 1 and false ==>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let age =45
+let stringmaker = String(age)
+console.log(stringmaker);
+console.log(typeof stringmaker);
